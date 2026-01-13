59972e7c6ca911a0669d45054d4e0e67daa33bc7
Eleboy08 <hmmacasaettobias@student.apc.edu.ph>
2026-01-13 06:52:04 +0000

    Task 03 Create a Clean Project

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <h1>{{ message }}</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      message: 'You did it horaay'
-    };
-  }
-};
-</script>
+<script></script>
 
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(33, 97, 235);
+  }
+</style>
